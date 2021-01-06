9f323cef82cb7bd07ab38676a951346fd698ecc8
JanColon <50427965+JanColon@users.noreply.github.com>
2021-01-06 07:32:28 -0500

    Some comments
    If it wasn't easy to read before, not it is. Hopefully.

--- citation.py
+++ citation.py
@@ -2,33 +2,40 @@ print ("Jan's Citation Machine")
 print ("Press ENTER key to continue")
 start_type = input()
 
+#The actual source of the citation 
 if start_type == (""):
 	print ("What's the Source? 'URL, Book, Etc.'")
 	url = input()
-
+#The date of the source
 if url == url:
 	print ("What's the date? 'Year'")
 	date = input()
 
+#The author/creator of the source
 if date == date:
 	print ("Who wrote/created the source? 'Name'")
 	author = input()
 
+#The source's title
 if author == author:
 	print ("What's the title of the source?")
 	title = input()	
 
+#The publisher of the source
 if title == title:
 	print ("What's the name of the association/company that made the source?")
 	comp = input()
 
+#An optional annotation for the citation
 if comp == comp:
 	print ("You want to write an annotation? If not, press ENTER to continue.")
 	next = input()
 
+#Just filler
 if next == next:
 	print ("Processed Bibliography... Press ENTER to view.")
 	finish = input()
 
+###--The finished citation--###
 if finish == finish:
 	print (author + "." + " " + title + "." + " " + comp + (".") + " " + date + (",") + " " + url + " " + next)
